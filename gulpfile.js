64fd9830ed40036b0240ff99329fede08f36cae9
niksmac <nikhil@lightrains.com>
2015-12-19 22:06:59 +0530

    Added Favicons

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,10 @@ var gulp = require('gulp');
 var autoprefix = require('gulp-autoprefixer');
 var minifyCSS = require('gulp-minify-css');
 var concat = require('gulp-concat');
+var favicons = require("gulp-favicons");
 
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
 
 // CSS concat, auto-prefix and minify
 gulp.task('styles', function() {
@@ -16,8 +19,7 @@ gulp.task('styles', function() {
   .pipe(gulp.dest('./public/css/'));
 });
 
-var imagemin = require('gulp-imagemin');
-var pngquant = require('imagemin-pngquant');
+
 
 gulp.task('images',  function() {
   gulp.src('./public/imgfull/*')
@@ -41,6 +43,22 @@ gulp.task('images',  function() {
   .pipe(gulp.dest('./public/img/stack'));
 });
 
+gulp.task('favicon', function() {
+  gulp.src("./public/favicons/pegke-logo.png").pipe(favicons({
+    appName: "Pegke Loyalty",
+    appDescription: "Customer Loyalty & Retention Management Software",
+    background: "#020307",
+    path: "/public/favicons/",
+    url: "https://pegke.com/",
+    display: "standalone",
+    orientation: "portrait",
+    version: 1.0,
+    logging: false,
+    online: false,
+    html: "./_includes/favicons.html",
+    replace: true
+  })).pipe(gulp.dest("./public/favicons/"));
+});
 // default gulp task
 gulp.task('default', ['styles', 'images'], function() {
 
